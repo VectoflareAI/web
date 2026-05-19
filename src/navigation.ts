9be477c0eaae39e51c4d378b7e071c3330fb0b11
Vectoflare <vectoflare@hotmail.com>
2026-05-19 18:30:37 +0800

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -170,9 +170,12 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: '#' },
+    { ariaLabel: 'Whatsapp', icon: 'tabler:brand-whatsapp', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/VectoflareAI/web' },
   ],
